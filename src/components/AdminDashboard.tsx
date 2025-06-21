a7f9019feb9e885ee64b64ce1407538a22608656
Swapnil-7 <chavanswap94@gmail.com>
2025-06-21 15:55:07 +0530

    changes in admindashboard

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -1,6 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
 import React, { useState } from 'react';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
 const AdminDashboard: React.FC = () => {
   const [adminPasswords, setAdminPasswords] = useState({
     adminPassword: '',
